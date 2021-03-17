d3e469de05f5f80ea2167c796bfe7368e1bc6767
ds <fp.ob>
2021-03-17 16:09:32 +0100

    easy

--- hotels.py
+++ hotels.py
@@ -203,7 +203,7 @@ def alternative(x,y):
         while True:
             try:
                 sp.browser.find_element_by_xpath('//*[@id="qf-1q-destination"]').send_keys(Keys.ENTER)
-                time.sleep(0.8)
+                time.sleep(12)
                 sp.browser.save_screenshot('testtemp.png')
             except:
                 break
