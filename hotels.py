a601dbfd02971f471ae5e9050b9f5f126ae82995
ds <fp.ob>
2021-03-17 15:38:10 +0100

    button click

--- hotels.py
+++ hotels.py
@@ -210,9 +210,14 @@ def alternative(x,y):
             button_=sp.browser.find_element_by_css_selector('.widget-query-geo > form:nth-child(2) > div:nth-child(4) > button:nth-child(1)')
             sp.browser.execute_script("arguments[0].scrollIntoView();", button_)
             button_.click()
-            time.sleep(2)
-            sp.browser.save_screenshot('testtemp.png')
-            myElem = WebDriverWait(sp.browser, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/main/div[4]/div[1]/div[1]/div/div[1]/h1')))
+            while True:
+                try:
+                    time.sleep(2)
+                    sp.browser.save_screenshot('testtemp.png')
+                    myElem = WebDriverWait(sp.browser, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/main/div[4]/div[1]/div[1]/div/div[1]/h1')))
+                except:
+                    button_.click()
+                    time.sleep(2)
         #sp.browser.execute_script("arguments[0].scrollIntoView();", button_)
         #button_.click()
         #sp.browser.find_element_by_xpath('//*[@id="qf-1q-destination"]').send_keys(Keys.ENTER)
