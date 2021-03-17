7f3aa06001df952f3f407d392ad238a770e9f5e7
ds <fp.ob>
2021-03-17 15:34:50 +0100

    test

--- hotels.py
+++ hotels.py
@@ -204,12 +204,15 @@ def alternative(x,y):
         time.sleep(0.3)
         try:
             sp.browser.find_element_by_xpath('//*[@id="qf-1q-destination"]').send_keys(Keys.ENTER)
-            myElem = WebDriverWait(sp.browser, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/main/div[4]/div[1]/div[1]/div/div[1]/h1')))
+            myElem = WebDriverWait(sp.browser, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/main/div[4]/div[1]/div[1]/div/div[1]/h1')))
 
         except:
             button_=sp.browser.find_element_by_css_selector('.widget-query-geo > form:nth-child(2) > div:nth-child(4) > button:nth-child(1)')
             sp.browser.execute_script("arguments[0].scrollIntoView();", button_)
+            button_.click()
             time.sleep(2)
+            sp.browser.save_screenshot('testtemp.png')
+            myElem = WebDriverWait(sp.browser, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/main/div[4]/div[1]/div[1]/div/div[1]/h1')))
         #sp.browser.execute_script("arguments[0].scrollIntoView();", button_)
         #button_.click()
         #sp.browser.find_element_by_xpath('//*[@id="qf-1q-destination"]').send_keys(Keys.ENTER)
