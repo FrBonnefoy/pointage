b639fefd933ba9afcc2281d83df305a9ab9508b9
ds <fp.ob>
2021-03-17 12:32:29 +0100

    user agent session 2

--- support.py
+++ support.py
@@ -194,7 +194,8 @@ def open_session2():
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument('start-maximized')
     chrome_options.add_argument('disable-infobars')
-    chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"')
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    chrome_options.add_argument('user-agent={0}'.format(user_agent))
     #chrome_options.add_extension('~/webrtc.crx')
     '''
     preferences = {
