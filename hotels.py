8711642cfefa57f65bd564a0fe922f369a5b1371
ds <fp.ob>
2021-03-19 14:17:14 +0100

    cosito

--- hotels.py
+++ hotels.py
@@ -62,6 +62,96 @@ def open_session_firefox():
 def close_session():
     browser.close()
 
+def pointer2_0(x):
+    ts = time.gmtime()
+    tsx=time.strftime("%s", ts)
+    namefile='hotels'+tsx+'.csv'
+    fhandle=open(namefile,'w', encoding="utf-8")
+    headers = ("Hotel Name"+"\t"+'stars'+'\t'+"Capacities" + '\t' + "webname" + '\t' + "address\n")
+    fhandle.write(headers)
+    fhandle.close()
+    lines = open(x, 'r').readlines()
+    pbar=tqdm(total=len(lines))
+    for line in lines:
+        scrape_hotel_info(line,namefile)
+
+def scrape_hotel_info(x,y):
+    try:
+        sp.req(x)
+        webpage=sp.page.text
+        toy_soup2 = soup(webpage, "html.parser")
+        gold=toy_soup2.find("div",{"id":"overview-section-4"})
+        if len(gold)<1:
+            gold=toy_soup2.find("div",{"class":"_2cVsY2"})
+        gold = str(gold)
+        chambres = re.findall(r"(\d+) chambres", gold)
+        if len(chambres)<1:
+            chambres = re.findall(r"(\d+) appartements", gold)
+        try:
+            chambres=chambres[0]
+        except:
+            chambres=''
+        try:
+            sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
+        except:
+            pass
+        silver=toy_soup2.find("span",{"class":"star-rating-text star-rating-text-strong widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
+        if len(silver)<1:
+            silver=toy_soup2.find("span",{"class":"_2dOcxA"})
+        silver2=toy_soup2.find("span",{"class":"star-rating-text widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
+        try:
+            silver = str(silver)
+            silver2 = str(silver2)
+            stars = re.findall(r"(\d?\,?\d) étoiles", silver)
+            stars2 = re.findall(r"(\d?\,?\d) étoiles", silver2)
+            if len(stars)==0 and len(stars2)>0:
+                stars=stars2[0]
+            else:
+                stars=stars[0]
+        except:
+            stars=''
+        bronze = toy_soup2.find("h2")
+        if len(bronze)<1:
+            bronze = toy_soup2.find("div",{'class':'_2h6Jhd'})
+        try:
+            bronze=str(bronze)
+            bronze=bronze.replace("<h2>","")
+            bronze=bronze.replace("</h2>","")
+            vname = bronze
+        except:
+            vname = ""
+        try:
+            sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
+        except:
+            pass
+        plastic = toy_soup2.find("span",{"class":"postal-addr"})
+        if len(plastic)<1:
+            plastic = toy_soup2.findAll("span",{"class":"_2wKxGq _1clhIX"})
+        try:
+            adrs = plastic.text
+        except:
+            adrs = ""
+
+        varlist=[x,stars,chambres,vname,adrs]
+        to_append=varlist
+        s = pd.DataFrame(to_append).T
+        s.to_csv(y, mode='a', header=False,sep='\t',index=False)
+        #time.sleep(1)
+        pbar.update(1)
+    except Exception as ex:
+        stars=''
+        chambres=''
+        vname=''
+        adrs=''
+        print(x, 'could not be completed','because of',ex)
+        varlist=[x,stars,chambres,vname,adrs]
+        to_append=varlist
+        s = pd.DataFrame(to_append).T
+        s.to_csv(y, mode='a', header=False,sep='\t',index=False)
+        #time.sleep(1)
+        pbar.update(1)
+
+
 # Read lines and put them into a list
 def original(y):
 
