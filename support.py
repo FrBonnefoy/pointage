8571dabba3db8f91af57e8d09645706fa0cb4f49
ds <fp.ob>
2021-03-19 15:10:00 +0100

    test

--- support.py
+++ support.py
@@ -265,7 +265,7 @@ class google_search_site:
         global page
         global google_url
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        page=requests.get(self.url,proxies=proxyDict2,verify=False,headers=headers)
+        sp.req2(self.url)
         description=scrape_light('div',{'class':'yuRUbf'})
         lecture=description.now()
         try:
