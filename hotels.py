c7772c93462e05dc66bd77a369ee203e4807b4ca
ds <fp.ob>
2021-03-19 15:42:56 +0100

    France

--- hotels.py
+++ hotels.py
@@ -81,7 +81,7 @@ def pointer2_0(x):
 def scrape_hotel_info(x,y):
     try:
         x=x.strip().replace('"','')
-        cosito=sp.google_search_site(x,'site:hotels.com').request()
+        cosito=sp.google_search_site(x,'site:fr.hotels.com').request()
         sp.req(cosito)
         webpage=sp.page.text
         toy_soup2 = soup(webpage, "html.parser")
