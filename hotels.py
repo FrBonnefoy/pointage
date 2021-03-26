6a740f3d7a067a772a988794b660159d082dc217
ds <fp.ob>
2021-03-26 12:45:43 +0100

    scrape hotel upgrade

--- hotels.py
+++ hotels.py
@@ -80,116 +80,122 @@ def pointer2_0(x):
         scrape_hotel_info(line,namefile)
 
 def scrape_hotel_info(x,y):
+    x=x.strip().replace('"','')
+    cosito=cs.custom_search(x)
     try:
-        x=x.strip().replace('"','')
-        cosito=cs.custom_search(x)
         cosito.request()
-
+    except Exception as e:
+        print(e)
+    try:
+        print('checking tripadvisor...')
         try:
-            url=cosito.hotels
+            url=cosito.tripadvisor
         except:
             url=''
-
-        sp.req(cosito.hotels)
+        sp.req2(url)
         webpage=sp.page.text
-        toy_soup2 = soup(webpage, "html.parser")
-        gold=toy_soup2.find("div",{"id":"overview-section-4"})
-        if len(gold)<1:
-            gold=toy_soup2.find("div",{"class":"_2cVsY2"})
-        gold = str(gold)
-        chambres = re.findall(r"(\d+) chambres", gold)
-        if len(chambres)<1:
-            chambres = re.findall(r"(\d+) appartements", gold)
+        trip_soup = soup(webpage, "html.parser")
+        stars_trip=trip_soup.findAll('svg',{'class':'AZd6Ff4E'})
+        capacity_trip=trip_soup.findAll('div',{'id':'ABOUT_TAB'})
+        name_trip=trip_soup.findAll('h1',{'id':'HEADING'})
+        adrs_trip=trip_soup.findAll('span',{'class':'_3ErVArsu jke2_wbp'})
+
         try:
-            chambres=chambres[0]
+            stars=str(stars_trip[0]['title']).replace(' sur 5\xa0bulles','')
+        except:
+            stars=''
+        try:
+            chambres=str(capacity_trip)
+            roomsy=re.compile('NOMBRE DE CHAMBRES<\/div><div class="_1NHwuRzF">(\d+)')
+            chambres=roomsy.findall(chambres)[0]
         except:
             chambres=''
         try:
-            sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
+            vname=name_trip[0].text
         except:
-            pass
-        silver=toy_soup2.find("span",{"class":"star-rating-text star-rating-text-strong widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
-        if len(silver)<1:
-            silver=toy_soup2.find("span",{"class":"_2dOcxA"})
-        silver2=toy_soup2.find("span",{"class":"star-rating-text widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
+            vname=''
         try:
-            silver = str(silver)
-            silver2 = str(silver2)
-            stars = re.findall(r"(\d?\,?\d) étoiles", silver)
-            stars2 = re.findall(r"(\d?\,?\d) étoiles", silver2)
-            if len(stars)==0 and len(stars2)>0:
-                stars=stars2[0]
-            else:
-                stars=stars[0]
+            adrs=adrs_trip[0].text
         except:
-            stars=''
-        bronze = toy_soup2.find("h2")
-        if len(bronze)<1:
-            bronze = toy_soup2.find("div",{'class':'_2h6Jhd'})
-        try:
-            bronze=str(bronze)
-            bronze=bronze.replace("<h2>","")
-            bronze=bronze.replace("</h2>","")
-            vname = bronze
-        except:
-            vname = ""
-        try:
-            sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
-        except:
-            pass
-        plastic = toy_soup2.find("span",{"class":"postal-addr"})
-        if len(plastic)<1:
-            plastic = toy_soup2.findAll("span",{"class":"_2wKxGq _1clhIX"})
-        try:
-            adrs = plastic.text
-        except:
-            adrs = ""
-
+            adrs=""
         varlist=[str(x).replace('\t',''),str(stars).replace('\t',''),str(chambres).replace('\t',''),str(vname).replace('\t',''),str(adrs).replace('\t',''),str(url).replace('\t','')]
         to_append=varlist
         s = pd.DataFrame(to_append).T
         s.to_csv(y, mode='a', header=False,sep='\t',index=False)
         #time.sleep(1)
         pbar.update(1)
-    except:
-        try:
-            print('checking tripadvisor...')
-            try:
-                url=cosito.tripadvisor
-            except:
-                url=''
-            sp.req2(cosito.tripadvisor)
-            webpage=sp.page.text
-            trip_soup = soup(webpage, "html.parser")
-            stars_trip=trip_soup.findAll('svg',{'class':'AZd6Ff4E'})
-            capacity_trip=trip_soup.findAll('div',{'id':'ABOUT_TAB'})
-            name_trip=trip_soup.findAll('h1',{'id':'HEADING'})
-            adrs_trip=trip_soup.findAll('span',{'class':'_3ErVArsu jke2_wbp'})
 
+    except Exception as e2
+        print(e2)
+        print('checking hotels.com...')
+        try:
+            url=cosito.hotels
+        except:
+            url=''
+
+        try:
+            sp.req(url)
+            webpage=sp.page.text
+            toy_soup2 = soup(webpage, "html.parser")
+            gold=toy_soup2.find("div",{"id":"overview-section-4"})
+            if len(gold)<1:
+                gold=toy_soup2.find("div",{"class":"_2cVsY2"})
+            gold = str(gold)
+            chambres = re.findall(r"(\d+) chambres", gold)
+            if len(chambres)<1:
+                chambres = re.findall(r"(\d+) appartements", gold)
             try:
-                stars=str(stars_trip[0]['title']).replace(' sur 5\xa0bulles','')
-            except:
-                stars=''
-            try:
-                chambres=str(capacity_trip)
-                roomsy=re.compile('NOMBRE DE CHAMBRES<\/div><div class="_1NHwuRzF">(\d+)')
-                chambres=roomsy.findall(chambres)[0]
+                chambres=chambres[0]
             except:
                 chambres=''
             try:
-                vname=name_trip[0].text
+                sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
             except:
-                vname=''
+                pass
+            silver=toy_soup2.find("span",{"class":"star-rating-text star-rating-text-strong widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
+            if len(silver)<1:
+                silver=toy_soup2.find("span",{"class":"_2dOcxA"})
+            silver2=toy_soup2.find("span",{"class":"star-rating-text widget-star-rating-overlay widget-tooltip widget-tooltip-responsive widget-tooltip-ignore-touch"})
             try:
-                adrs=adrs_trip[0].text
+                silver = str(silver)
+                silver2 = str(silver2)
+                stars = re.findall(r"(\d?\,?\d) étoiles", silver)
+                stars2 = re.findall(r"(\d?\,?\d) étoiles", silver2)
+                if len(stars)==0 and len(stars2)>0:
+                    stars=stars2[0]
+                else:
+                    stars=stars[0]
             except:
-                adrs=""
+                stars=''
+            bronze = toy_soup2.find("h2")
+            if len(bronze)<1:
+                bronze = toy_soup2.find("div",{'class':'_2h6Jhd'})
+            try:
+                bronze=str(bronze)
+                bronze=bronze.replace("<h2>","")
+                bronze=bronze.replace("</h2>","")
+                vname = bronze
+            except:
+                vname = ""
+            try:
+                sp.browser.find_element_by_css_selector('.cta.widget-overlay-close').click()
+            except:
+                pass
+            plastic = toy_soup2.find("span",{"class":"postal-addr"})
+            if len(plastic)<1:
+                plastic = toy_soup2.findAll("span",{"class":"_2wKxGq _1clhIX"})
+            try:
+                adrs = plastic.text
+            except:
+                adrs = ""
+
             varlist=[str(x).replace('\t',''),str(stars).replace('\t',''),str(chambres).replace('\t',''),str(vname).replace('\t',''),str(adrs).replace('\t',''),str(url).replace('\t','')]
             to_append=varlist
             s = pd.DataFrame(to_append).T
             s.to_csv(y, mode='a', header=False,sep='\t',index=False)
             #time.sleep(1)
             pbar.update(1)
+
 
         except Exception as ex:
             stars=''
