34858614e9af390bbaf947ad942e11ccca884790
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-05-17 16:27:43 +0200

    largescreen

--- support.py
+++ support.py
@@ -217,7 +217,7 @@ def open_session2():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--proxy-server=%s' % PROXY)
-    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--window-size=1920x10080")
     chrome_options.add_argument('start-maximized')
     chrome_options.add_argument('disable-infobars')
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
