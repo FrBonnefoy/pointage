754becc66cf8f2ebbb5db2baf87015e19f8f0bfd
ds <fp.ob>
2021-06-27 02:39:34 +0200

    sleeper

--- adresses.py
+++ adresses.py
@@ -165,7 +165,7 @@ def obtain(x):
     print('Fetching location data...')
 
     for a_data in tqdm(df_list):
-        time.sleep(random.randint(5,120))
+        time.sleep(random.randint(10,30))
         a_data['DATA'] = a_data.apply(lambda x: gc.searcher_detail(x[a_data.columns[0]]).data, axis=1)
 
 
