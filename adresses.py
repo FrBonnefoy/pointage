cd6fb6a232ded50e31581ffb7be718deb8b52d53
ds <fp.ob>
2021-07-26 11:16:13 +0200

    import additional features

--- adresses.py
+++ adresses.py
@@ -13,11 +13,12 @@ import jellyfish as js
 import concurrent.futures
 import time
 import random
+from adrs_modules import additional_features as _add
 
 # Codifies names according to phonetics
 
 def send_codex(x):
-    rating_codex1 = [js.match_rating_codex(y) for y in x.split()]
+    rating_codex1 = [js.mch_rating_codex(y) for y in x.split()]
     return ''.join(rating_codex1)
 
 # Flags strings that are very dissimilar
